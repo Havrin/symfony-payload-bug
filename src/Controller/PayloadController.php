f7f7b37e847533fc828477aeef6839dae97f4f8f
Frederik Holz <holz@auxmoney.com>
2023-12-05 15:49:29 +0100

    chore: Add non nullable route

--- src/Controller/PayloadController.php
+++ src/Controller/PayloadController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\DTO\ChangePassword;
+use App\DTO\ChangePasswordWithoutNull;
 use App\DTO\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,6 +22,12 @@ class PayloadController extends AbstractController
 
     #[Route(path: 'api/change-password', name: 'change-password', methods: ['POST'], format: 'json')]
     public function changePasswordAction(#[MapRequestPayload] ChangePassword $changePassword): JsonResponse
+    {
+        return new JsonResponse($changePassword->getEmail());
+    }
+
+    #[Route(path: 'api/change-password-without-null', name: 'change-password-without-null', methods: ['POST'], format: 'json')]
+    public function changePasswordWithoutNullAction(#[MapRequestPayload] ChangePasswordWithoutNull $changePassword): JsonResponse
     {
         return new JsonResponse($changePassword->getEmail());
     }
---
+++ src/DTO/ChangePasswordWithoutNull.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class ChangePasswordWithoutNull
+{
+    public function __construct(
+        #[Assert\NotNull,
+            Assert\NotBlank,
+            Assert\Email]
+        private string $email,
+        #[Assert\NotNull,
+            Assert\NotBlank]
+        private string $password,
+        #[SerializedName(serializedName: 'old_password')]
+        private string $oldPassword
+    ) {
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getOldPassword(): string
+    {
+        return $this->oldPassword;
+    }
+}
